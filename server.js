d8a2a211b2f3e5cd73d54e3010af45fd38ca8eed
vucat2000 <cnikolai@earthlink.net>
2019-04-24 06:59:52 -0400

    touch ups

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
   // Starts the server to begin listening
   // =============================================================
